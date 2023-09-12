56983f4d20195159f7bb1ae413b1e18193e6ab61
Thomas Kaae <tka@it-minds.dk>
2023-09-12 11:54:42 +0200

    Added props + types to sidebaritem component

--- src/components/sidebaritem.tsx
+++ src/components/sidebaritem.tsx
@@ -11,11 +11,11 @@ export const SidebarItem = ({ to, text, icon }: SidebarProps) => {
   return (
     <li>
       <Link
-        to="/marketplace"
+        to={to}
         className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
       >
-        <BuildingStorefrontIcon className="h-6 w-6" />
-        <span className="ml-3 text-xl">Marketplace</span>
+        {icon}
+        <span className="ml-3 text-xl">{text}</span>
       </Link>
     </li>
   );
