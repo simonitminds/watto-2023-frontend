01aadc208fd467a2ca83ef4b3af1aab9f8a238f2
Thomas Kaae <tka@it-minds.dk>
2023-09-12 12:47:30 +0200

    Finished sidebaritem component  with types and mapped in sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { SidebarItem, SidebarProps } from './sidebaritem';
 import {
   BuildingStorefrontIcon,
   UserIcon,
@@ -7,6 +7,20 @@ import {
 } from '@heroicons/react/24/solid';
 
 export const Sidebar = () => {
+  const sidebarItems: SidebarProps[] = [
+    {
+      to: '/marketplace',
+      icon: <BuildingStorefrontIcon />,
+      text: 'Marketplace',
+    },
+    { to: '/owneditems', icon: <UserIcon />, text: 'Owned items' },
+    {
+      to: '/transactions',
+      icon: <ArchiveBoxIcon />,
+      text: 'Transactions',
+    },
+  ];
+
   return (
     <aside
       id="default-sidebar"
@@ -14,40 +28,11 @@ export const Sidebar = () => {
       aria-label="Sidebar"
     >
       <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-        <ul className="hover:font-semibold">
-          <li>
-            <Link
-              to="/marketplace"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <BuildingStorefrontIcon className="h-6 w-6" />
-              <span className="ml-3 text-xl">Marketplace</span>
-            </Link>
-          </li>
-        </ul>
-        <ul className="hover:font-semibold">
-          <li className="mt-1">
-            <Link
-              to="/marketplace"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <UserIcon className="h-6 w-6" />
-              <span className="ml-3 text-xl">Owned items</span>
-            </Link>
-          </li>
-        </ul>
-        <ul className="hover:font-semibold">
-          <li className="mt-1">
-            <Link
-              to="/marketplace"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <ArchiveBoxIcon className="h-6 w-6" />
-
-              <span className="ml-3 text-xl">Transactions</span>
-            </Link>
-          </li>
-        </ul>
+        {sidebarItems.map((item, index) => (
+          <ul key={index} className="hover:font-semibold">
+            <SidebarItem to={item.to} icon={item.icon} text={item.text} />
+          </ul>
+        ))}
       </div>
     </aside>
   );
--- src/components/sidebaritem.tsx
+++ src/components/sidebaritem.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
-type SidebarProps = {
+export type SidebarProps = {
   to: string;
   text: string;
   icon: ReactNode;
@@ -14,7 +14,7 @@ export const SidebarItem = ({ to, text, icon }: SidebarProps) => {
         to={to}
         className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
       >
-        {icon}
+        <div className="h-6 w-6"> {icon}</div>
         <span className="ml-3 text-xl">{text}</span>
       </Link>
     </li>
